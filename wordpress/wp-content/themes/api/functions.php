76eeac8cc1c9346feda263c9adb59f8d205f0396
theluanz <luanzuffo.lz@gmail.com>
2022-03-05 18:31:13 -0300

    jwt token

--- wordpress/wp-content/themes/api/functions.php
+++ wordpress/wp-content/themes/api/functions.php
@@ -1,12 +1,19 @@
-<?php 
+<?php
 remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
 $dirbase = get_template_directory();
-require_once ($dirbase . '/endpoints/user_post.php');
+require_once($dirbase . '/endpoints/user_post.php');
 
-function change_api() { 
-  return 'api'; 
-} 
+function change_api()
+{
+  return 'api';
+}
 
-add_filter( 'rest_url_prefix', 'change_api');
-?>
+add_filter('rest_url_prefix', 'change_api');
+
+function expire_token()
+{
+  return time() + (60 * 60 * 24);
+}
+
+add_action('jwt_auth_expire', 'expire_token');
