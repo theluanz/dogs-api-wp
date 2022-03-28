5c4c65eaf9954161228459ac1d7113474eda6640
theluanz <luanzuffo.lz@gmail.com>
2022-03-27 23:47:14 -0300

    stats of my posts

--- wordpress/wp-content/themes/api/functions.php
+++ wordpress/wp-content/themes/api/functions.php
@@ -21,6 +21,8 @@ require_once($dirbase . '/endpoints/comment_post.php');
 
 require_once($dirbase . '/endpoints/password.php');
 
+require_once($dirbase . '/endpoints/stats.php');
+
 update_option('large_size_w', 1000);
 update_option('large_size_h', 1000);
 update_option('large_crop', 1);
